b1d379cafd041b80ffc263bf72d529cdecb22d1f
KhOlga <kharabet.olga@gmail.com>
2019-10-05 22:47:31 +0300

    Update db_config.php

--- database/db_config.php
+++ database/db_config.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_config = array(
-    'database_dsn'  => 'mysql:dbname=d309projtlv2io;host=ec2-79-125-2-142.eu-west-1.compute.amazonaws.com',
-    'database_user' => 'wdybeydemvcnar',
-    'database_pass' => '603afdcbc7caa4f19ab32fec171acb777339a6397c535a3cb0c37c11bc1dc438',
+    'database_dsn'  => '...',
+    'database_user' => '...',
+    'database_pass' => '...',
 );
 return $db_config;
